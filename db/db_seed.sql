f9f279191e4b66ed50a14efa19617eadcb38a865
Rajeev N Bharshetty <rajeevrvis@gmail.com>
2016-05-10 10:15:54 +0530

    Add some dealers to the seed file (#12)

--- db/db_seed.sql
+++ db/db_seed.sql
@@ -10,6 +10,21 @@ $body$
       PERFORM dblink_exec('seed_session', 
         $$COPY users (emp_id, name, phone_no, email) FROM './employees.csv' DELIMITER ',' CSV HEADER;$$);
 
+    PERFORM dblink_exec('seed_session', $$INSERT into dealers (name, icon)
+        values ('Amazon', 'some-url');$$);
+
+    PERFORM dblink_exec('seed_session', $$INSERT into dealers (name, icon)
+        values ('Fedex', 'some-url');$$);
+
+    PERFORM dblink_exec('seed_session', $$INSERT into dealers (name, icon)
+        values ('Flipkart', 'some-url');$$);
+
+    PERFORM dblink_exec('seed_session', $$INSERT into dealers (name, icon)
+        values ('Bluedart', 'some-url');$$);
+
+    PERFORM dblink_exec('seed_session', $$INSERT into dealers (name, icon)
+        values ('Myntra', 'some-url');$$);
+
     PERFORM dblink_disconnect('seed_session');
 END;
 $body$
